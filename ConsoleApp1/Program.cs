90a116c1ff93fe60a259a8ddf46857c90a8f482c
Keith Burnell <kburnell@skylinetechnologies.com>
2016-05-02 09:26:17 -0500

    Initial push of code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConsoleApp1 {
+
+    internal class Program {
+
+        private static void Main(string[] args) {
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("SharedValue1"));
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("SharedValue2"));
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("ValueFromAppConfig"));
+            Console.ReadKey();
+        }
+
+    }
+
+}
